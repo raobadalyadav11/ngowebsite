d3b5898a929e7f699ec55b45cbba33f77f42f62e
Badal Kumar <bkbadal7779810465@gmail.com>
2025-02-26 18:43:22 +0530

    ngowebsite init

---
+++ src/main/java/com/ngowebsite/ngowebsite/NgowebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ngowebsite.ngowebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgowebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgowebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngowebsite/ngowebsite/NgowebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngowebsite.ngowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgowebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
